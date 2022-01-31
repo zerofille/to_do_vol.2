3230f405393b84363bf99dc25f8ffb7e6ebd61ae
zerofille <nstrdnv@gmail.com>
2022-01-31 12:13:03 +0200

    Add rtk query for fetching tasks

--- src/Components/TaskList/TaskList.tsx
+++ src/Components/TaskList/TaskList.tsx
@@ -1,40 +1,23 @@
-import React, { useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from '../../app/hooks'
+import React from 'react';
 import Task from '../Task/Task';
-import { getTask } from '../../app/taskSlice';
 import { IData } from '../../app/taskSlice';
 import { useGetTasksQuery } from '../../rtkQuery/rtqApi';
 
+
 function TaskList() {
-  const dispatch = useAppDispatch();
-  const tasksArr = useAppSelector((state) => state.task.data)
   const {data, isLoading, isSuccess, isError, error} = useGetTasksQuery();
-  console.log(data)
-  // useEffect(() => {
-  //   dispatch(getTask())
-  // }, []);
   return (
     <>
       <ul>
-
-        {
-          isSuccess ?
-            {
-              if(data: IData[]) {
-                {
-                  data.length !== 0 ? data.map((elem: IData) => {
-                    return <Task key={elem.id} id={elem.id} title={elem.title} text={elem.text}
-                                 task_status={elem.task_status}/>
-                  }) : <h1>no tasks yet</h1>
-                }
-              }
-            }
-            : <p>loading...</p>}
-
-
+        {error ? (
+          <>Error occurred</>
+        ) : isLoading ? (
+          <>Loading...</>
+        ) : data ? data.map((e: IData) => {
+          return <Task key={e.id} id={e.id} title={e.title} text={e.text} task_status={e.task_status}/>
+        }) : null}
       </ul>
     </>
   )
 }
-
 export default TaskList;
--- src/rtkQuery/rtqApi.ts
+++ src/rtkQuery/rtqApi.ts
@@ -1,11 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { IData } from '../app/taskSlice';
 
+
+type DataArr = [IData]
 
 export const apiSlice = createApi({
   reducerPath: 'taskApi',
   baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3000'}),
   endpoints: builder => ({
-    getTasks: builder.query<void, void>({
+    getTasks: builder.query<DataArr, void>({
       query: () => '/tasks'
     })
   })
